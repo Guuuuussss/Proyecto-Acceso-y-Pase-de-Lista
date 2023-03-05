ea2834af3ab2c88c440a25389b0b8106a36b8297
Guuuuussss <gustavo.luna@gmail.com>
2023-03-05 15:03:50 -0600

    ventana de inicio de sesion terminada, falta validad el usuario y contraseña con la bd, tambien se hizo en el master panel el seelct de toda la info de la bd

--- Interfaz/forms/form_login.py
+++ Interfaz/forms/form_login.py
@@ -6,13 +6,63 @@ from forms.form_master import MasterPanel
 
 
 class App:
+
+    def verificar(self):
+        usu = self.usuario.get()
+        password = self.password.get()
+        if(usu == "root" and password == "1234"):
+            self.ventana.destroy()
+            MasterPanel()
+        else:
+            messagebox.showerror(message="La contraseña no es correcta",title="Mensaje")
+
+
     def __init__(self):
         self.ventana = tk.Tk()
-        self.ventana.title('Inicio de sesion')
+        self.ventana.title('Sistema de Acceso')
         self.ventana.geometry('800x500')
         self.ventana.config(bg='#fcfcfc')
         self.ventana.resizable(width=0,height=0)
         utl.centrar_ventana(self.ventana,800,500)
-        self.ventana.mainloop()
 
-        logo = utl.leer_imagen("Interfaz\images\logo.png", (200,200))
+        logo = utl.leer_imagen("Proyecto-Acceso-y-Pase-de-Lista/Interfaz/images/logo.png", (200,200))
+
+        # Frame_logo
+
+        frame_logo = tk.Frame(self.ventana, bd=0, width=300, relief=tk.SOLID, padx=10, pady=10, bg='#EC3F45')
+        frame_logo.pack(side="left", expand=tk.NO, fill=tk.BOTH)
+        label = tk.Label(frame_logo, image=logo,bg='#EC3F45')
+        label.place(x=0,y=0,relwidth=1,relheight=1)
+
+        # frame_form
+        frame_form = tk.Frame(self.ventana, bd=0, relief=tk.SOLID, bg='#fcfcfc')
+        frame_form.pack(side="left", expand=tk.YES, fill=tk.BOTH)
+
+        # frame_form_top
+        frame_form_top = tk.Frame(frame_form, height=50, bd=0, relief=tk.SOLID,bg='black')
+        frame_form_top.pack(side="top",fill=tk.X)
+        title = tk.Label(frame_form_top,text="Inicio de sesión", font=('Times',30), fg="black",bg='#fcfcfc', pady=50)
+        title.pack(expand=tk.YES,fill=tk.BOTH)
+
+        # frame_form_fill
+        frame_form_fill = tk.Frame(frame_form,height=50, bd=0, relief=tk.SOLID, bg='#fcfcfc')
+        frame_form_fill.pack(side="bottom",expand=tk.YES,fill=tk.BOTH)
+
+        # etiquetas, entrys y boton
+        etiqueta_usuario = tk.Label(frame_form_fill, text="Número de cuenta", font=('Times',14), fg="black",bg='#fcfcfc', anchor="w")
+        etiqueta_usuario.pack(fill=tk.X, padx=20,pady=5)
+        self.usuario = ttk.Entry(frame_form_fill, font=('Times',14),width=20)
+        self.usuario.pack(fill=None, padx=20,pady=10,anchor="w")
+
+        etiqueta_password = tk.Label(frame_form_fill, text="Contraseña", font=('Times',14), fg="black",bg='#fcfcfc', anchor="w")
+        etiqueta_password.pack(fill=tk.X, padx=20,pady=10)
+        self.password = ttk.Entry(frame_form_fill, font=('Times',14),width=20)
+        self.password.pack(fill=None, padx=20,pady=10,anchor="w")
+        self.password.config(show="*")
+
+        inicio = tk.Button(frame_form_fill, text="Iniciar sesión", font=('Times', 15), bd=0,bg='red',width=20,command=self.verificar)
+        inicio.pack(fill=None, padx=20, pady=20)
+        inicio.bind("<Return>",(lambda event: self.verificar()))
+
+    
+        self.ventana.mainloop()
--- Interfaz/forms/form_master.py
+++ Interfaz/forms/form_master.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter.font import BOLD
 import util.generic as utl
+import mysql.connector
 
 class MasterPanel: 
     def __init__(self):
@@ -9,10 +10,33 @@ class MasterPanel:
         w, h = self.ventana.winfo_screenwidth(), self.ventana.winfo_screenheight()
         self.ventana.geometry("%dx%d+0+0" % (w,h))
         self.ventana.config(bg = '#fcfcfc')
-        self.ventana.resizable(width=0, height=0)
+        self.ventana.resizable(width=True, height=True)
 
-        logo = utl.leer_imagen("Interfaz\images\logo.png", (200,200))
+        logo = utl.leer_imagen("Proyecto-Acceso-y-Pase-de-Lista/Interfaz/images/logo.png", (200,200))
 
-        label = tk.Label(self.ventana, image=logo,bg='#3a7ff6')
+        label = tk.Label(self.ventana, image=logo,bg='#EC3F45')
         label.place(x=0,y=0,relwidth=1,relheight=1)
+        
+        # Conectar a la base de datos
+        self.conexion = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='Briza_3121',
+            database='accesos'
+        )
+        
+        # Obtener los registros de la tabla
+        cursor = self.conexion.cursor()
+        cursor.execute('SELECT * FROM accesos.usuarios LEFT JOIN accesos ON usuarios.identificador = accesos.identificador LEFT JOIN clases ON usuarios.identificador = clases.id_profesor WHERE usuarios.identificador = 374203472;')
+        registros = cursor.fetchall()
+        
+        # Crear la tabla para mostrar los registros
+        self.tabla = tk.Frame(self.ventana)
+        self.tabla.pack(fill=tk.BOTH, expand=True)
+        
+        for i, fila in enumerate(registros):
+            for j, valor in enumerate(fila):
+                celda = tk.Label(self.tabla, text=str(valor))
+                celda.grid(row=i, column=j)
+        
         self.ventana.mainloop()
